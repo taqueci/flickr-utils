a3ad3f9a53826338d07086e87dbc1a9a124b0d5b
taqueci <taqueci.n@gmail.com>
2014-10-17 00:33:19 +0900

    Read API key, secret and token from options, environment variables or config file.

--- flickr-album-import.pl
+++ flickr-album-import.pl
@@ -4,7 +4,7 @@ flickr-album-import.pl - upload photos and create a new photoset
 
 =head1 SYNOPSIS
 
-    flickr-album-import.pl [OPTION] ... KEY SECRET TOKEN TITLE PATH ...
+    flickr-album-import.pl [OPTION] ... TITLE PATH ...
 
 =head1 DESCRIPTION
 
@@ -14,9 +14,31 @@ If PATH is a directory, it uplaods files under PATH recursively.
 
 The first file found in PATH is used as primary photo of the album.
 
+The API key, secret and authentication token are specified by options,
+environment variables or a configuration file.
+
+~/.flickrrc is used as a default configuration file.
+The format is the same as flickr_upload's one.
+
 =head1 OPTIONS
 
 =over 4
+
+=item --key=KEY
+
+Use KEY as an API key.
+
+=item --secret=SECRET
+
+Use SECRET as a secret.
+
+=item --token=TOKEN
+
+Use TOKEN as an auth token.
+
+=item --rc=FILE
+
+Read FILE as rc file.
 
 =item -d DESC, --description=DESC
 
@@ -75,18 +97,29 @@ my $uri = 'https://api.flickr.com/services/upload/';
 my $rest_uri = 'https://api.flickr.com/services/rest/';
 my $auth_uri = 'https://api.flickr.com/services/auth/';
 
+my $rc = "$ENV{HOME}/.flickrrc";
+
 my %opt;
-GetOptions(\%opt, "description|d=s", "tag|t=s@", "sort|s", "keep-going|k",
-		   "log|l=s", "verbose", "help") || exit 1;
+GetOptions(\%opt, 'key=s', 'secret=s', 'token=s', 'rc=s',
+		   'description|d=s', 'tag|t=s@', 'sort|s', 'keep-going|k',
+		   'log|l=s', 'verbose', 'help') || exit 1;
 
 p_set_log($opt{log}) if defined($opt{log});
 p_set_verbose(1) if $opt{verbose};
 
 pod2usage(-exitval => 0, -verbose => 2, -noperldoc => 1) if $opt{help};
 
-(@ARGV > 3) || p_error_exit(1, "Too few arguments");
+(@ARGV > 1) || p_error_exit(1, "Too few arguments");
 
-my ($key, $secret, $token, $title, @path) = @ARGV;
+my ($title, @path) = @ARGV;
+
+my $config = read_config($opt{rc} // $ENV{FLICKR_RC} // $rc);
+
+my $key = $opt{key} // $ENV{FLICKR_KEY} // $config->{key};
+my $secret = $opt{secret} // $ENV{FLICKR_SECRET} // $config->{secret};
+my $token = $opt{token} // $ENV{FLICKR_TOKEN} // $config->{auth_token};
+
+check_values('key' => $key, 'secret' => $secret, 'token' => $token) || exit 1;
 
 my $api = Flickr::Upload->new({key => $key, secret => $secret,
 							   rest_uri => $rest_uri, auth_uri => $auth_uri});
@@ -100,7 +133,7 @@ my $file = find_files(@path) || exit 1;
 
 p_verbose("Uploading photos");
 my $photo_id = upload_photos($api, $token, $uri, $file, $opt{tag}, $desc,
-							 $opt{"keep-going"}) || exit 1;
+							 $opt{'keep-going'}) || exit 1;
 
 my $primary = shift @$photo_id;
 
@@ -114,6 +147,48 @@ p_verbose("Completed!\n");
 
 exit 0;
 
+
+sub read_config {
+	my $file = shift;
+	my $fh;
+	my %config;
+
+	return {} unless -f $file;
+
+	unless (open($fh, $file)) {
+		p_error("$file: $!");
+		return undef;
+	}
+
+	while (my $line = <$fh>) {
+		chomp $line;
+
+		# Remove comment.
+		$line =~ s/#.*$//;
+
+		my ($key, $val) = ($line =~ /^\s*(\w+)=(.+)\s*$/);
+
+		$config{$key} = $val if defined($key) && defined($val);
+	}
+
+	close($fh);
+
+	return \%config;
+}
+
+sub check_values {
+	my %value = @_;
+	my $nerr = 0; # Number of errors.
+
+	while (my ($key, $val) = each %value) {
+		unless (defined($val)) {
+			p_error("Undefined value for $key");
+			$nerr++;
+		}
+	}
+
+	return $nerr == 0;
+}
 
 sub find_files {
 	my @path = @_;
@@ -158,7 +233,7 @@ sub upload_photos {
 		push(@photo_id, $id);
 	}
 
-	return ($nerr == 0) ? \@photo_id : undef;
+	return (($nerr == 0) && (@photo_id > 0)) ? \@photo_id : undef;
 }
 
 sub upload_file {
